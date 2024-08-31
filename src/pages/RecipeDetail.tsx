5db8628abeadb8d5525a84d10b3bd92954453e2f
dlat.th <dlat.th45@gmail.com>
2024-08-31 22:12:22 +0900

    💄 style: Adjust layout for image and recipe name placement
    - Modified the layout to improve the positioning of images and recipe names
    - Ensured consistent alignment and spacing between elements

--- src/pages/RecipeDetail.tsx
+++ src/pages/RecipeDetail.tsx
@@ -21,29 +21,33 @@ const RecipeDetail: React.FC = () => {
 
   return (
     <div className="recipe-detail">
+      {/* 왼쪽 섹션의 내용 */}
       <div className="left-section">
-        {/* 왼쪽 섹션의 내용 */}
-        <img className="recipe-image" src={recipe.images[0]} alt={recipe.name} />
-        <h1 className="recipe-name">{recipe.name}</h1>
-        <div className="recipe-tags">
-          {recipe.tags.map((tag: string, index: number) => (
-            <button key={index} className="tag-button">
-              {tag}
-            </button>
-          ))}
+        <div className="left-header">
+          <h1 className="recipe-name">{recipe.name}</h1>
+          <img className="recipe-image" src={recipe.images[0]} alt={recipe.name} />
         </div>
-        <div className="recipe-ingredients">
-          <h2 className="recipe-ingredients-title">재료</h2>
-          <ul>
-            {recipe.ingredient.map((ingredient: string, index: number) => (
-              <li key={index}>{ingredient}</li>
+        <div className="left-info">
+          <div className="recipe-tags">
+            {recipe.tags.map((tag: string, index: number) => (
+              <button key={index} className="tag-button">
+                {tag}
+              </button>
             ))}
-          </ul>
-          <div className="recipe-description">{recipe.description}</div>
+          </div>
+          <div className="recipe-ingredients">
+            <h2 className="recipe-ingredients-title">재료</h2>
+            <ul>
+              {recipe.ingredient.map((ingredient: string, index: number) => (
+                <li key={index}>{ingredient}</li>
+              ))}
+            </ul>
+            <div className="recipe-description">{recipe.description}</div>
+          </div>
         </div>
       </div>
+      {/* 오른쪽 섹션의 내용 */}
       <div className="right-section">
-        {/* 오른쪽 섹션의 내용 */}
         <div className="recipe-text">{recipe.text}</div>
         <div className="carousel">
           <button onClick={prevSlide} className="carousel-control prev">
